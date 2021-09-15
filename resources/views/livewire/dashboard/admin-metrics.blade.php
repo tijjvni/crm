e7501ed7a862bc00ef3e4eb4e57c9abd5d71266d
tjyusuf <tjyusuf@Yahoo.com>
2021-09-15 16:13:36 +0000

    updates

--- resources/views/livewire/dashboard/admin-metrics.blade.php
+++ resources/views/livewire/dashboard/admin-metrics.blade.php
@@ -6,22 +6,27 @@
     </div>    
 
     <div class="flex flex-wrap space-y-2 space-x-2">
+
+    
         <div class=" flex-1 flex-col-1 divide-y divide-gray-light bg-white rounded-md shadow ">
             <div class="flex-col space-y-1 py-4 px-2 h-24">
                 <p class="text-gray-light text-base">Tickets</p>
                 <p class="text-lg text-gray">{{ count($tickets) }}</p>
             </div>
             <div class="flex p-2 bg-gray-50 text-primary h-8 rounded-b-md items-end">
+
                 <div class="flex items-center justify-between text-sm">
                     <div class="w-0 flex-1 flex items-center">
+                        <!-- Heroicon name: solid/paper-clip -->
                         <i class="fa fa-plus"></i>
                         <span class="ml-2 flex-1 w-0 truncate">
                             Create new
                         </span>
                     </div>    
-                </div>            
+                </div>                
+
             </div>
-        </div>
+        </div>        
         <div class=" flex-1 flex-col-1 divide-y divide-gray-light bg-white rounded-md shadow ">
             <div class="flex-col space-y-1 py-4 px-2 h-24">
                 <p class="text-gray-light text-base">Admins</p>
