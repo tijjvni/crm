08ed4307a052e01ec982896bd407e065fa143dee
tjyusuf <tjyusuf@Yahoo.com>
2021-09-15 15:52:36 +0000

    updates

--- app/Http/Livewire/Dashboard/AdminMetrics.php
+++ app/Http/Livewire/Dashboard/AdminMetrics.php
@@ -5,14 +5,19 @@ namespace App\Http\Livewire\Dashboard;
 use Livewire\Component;
 
 use App\Models\User;
+use App\Models\Ticket;
 
 class AdminMetrics extends Component
 {
 
     public $users; 
+    public $admins; 
+    public $tickets; 
 
     public function mount(){
         $this->users = User::all();
+        $this->admins = User::where('is_admin',true)->get();
+        $this->tickets = Ticket::all();
     }
 
     public function render()
--- resources/views/livewire/dashboard/admin-metrics.blade.php
+++ resources/views/livewire/dashboard/admin-metrics.blade.php
@@ -1,13 +1,40 @@
-<div class="my-2">
+<div class="flex-col my-2">
+    <div class="flex">
+        <p class="text-lg text-gray">
+            <i class="fa fa-chart"></i> Admin
+        </p>
+    </div>    
+
     <div class="flex flex-wrap space-y-2 space-x-2">
+        <div class=" flex-1 flex-col-1 divide-y divide-gray-light bg-white rounded-md shadow self-center">
+            <div class="flex-col space-y-1 py-4 px-2 h-24">
+                <p class="text-gray-light text-base">Tickets</p>
+                <p class="text-lg text-gray">{{ count($tickets) }}</p>
+            </div>
+            <div class="flex p-2 bg-gray-50 text-primary h-8 rounded-b-md items-end">
+                <p class="text-primary text-sm">Create new</p>
+            </div>
+        </div>
+        <div class=" flex-1 flex-col-1 divide-y divide-gray-light bg-white rounded-md shadow self-center">
+            <div class="flex-col space-y-1 py-4 px-2 h-24">
+                <p class="text-gray-light text-base">Admins</p>
+                <p class="text-lg text-gray">{{ count($admins) }}</p>
+            </div>
+            <div class="flex p-2 bg-gray-50 text-primary h-8 rounded-b-md items-end">
+                <p class="text-primary text-sm">Grant permission</p>
+            </div>
+        </div>
         <div class=" flex-1 flex-col-1 divide-y divide-gray-light bg-white rounded-md shadow self-center">
             <div class="flex-col space-y-1 py-4 px-2 h-24">
                 <p class="text-gray-light text-base">Users</p>
                 <p class="text-lg text-gray">{{ count($users) }}</p>
             </div>
-            <div class="flex p-2 bg-gray-50 text-primary h-8 rounded-b-md">
+            <div class="flex p-2 bg-gray-50 text-primary h-8 rounded-b-md items-end">
                 <p class="text-primary text-sm">View users</p>
             </div>
         </div>
     </div>
+
+
+
 </div>
