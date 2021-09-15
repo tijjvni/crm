193bd6991542cc65cc9efe78516c0fae68ba17ac
tjyusuf <tjyusuf@Yahoo.com>
2021-09-15 14:57:36 +0000

    updates

--- resources/views/livewire/dashboard/admin-metrics.blade.php
+++ resources/views/livewire/dashboard/admin-metrics.blade.php
@@ -1,16 +1,6 @@
-<div class="flex flex-wrap space-2">
+<div class="flex flex-wrap gap-2">
 
-    <div class=" flex-1 flex-col-1 divide-y divide-gray-lightbg-white rounded shadow-md self-center">
-        <div class="flex-col space-y-1 py-4 px-2 h-24">
-            <p class="text-gray-light text-base">Users</p>
-            <p class="text-lg text-gray">{{ count($users) }}</p>
-        </div>
-        <div class="flex p-2 bg-gray-50 text-primary h-8">
-            <p class="text-primary text-sm">View users</p>
-        </div>
-    </div>
-    
-    <div class=" flex-1 flex-col-1 divide-y divide-gray-lightbg-white rounded shadow-md self-center">
+    <div class=" flex-1 flex-col-1 divide-y divide-gray-light bg-white rounded-lg shadow-md self-center">
         <div class="flex-col space-y-1 py-4 px-2 h-24">
             <p class="text-gray-light text-base">Users</p>
             <p class="text-lg text-gray">{{ count($users) }}</p>
@@ -20,27 +10,4 @@
         </div>
     </div>
 
-    
-    <div class=" flex-1 flex-col-1 divide-y divide-gray-lightbg-white rounded shadow-md self-center">
-        <div class="flex-col space-y-1 py-4 px-2 h-24">
-            <p class="text-gray-light text-base">Users</p>
-            <p class="text-lg text-gray">{{ count($users) }}</p>
-        </div>
-        <div class="flex p-2 bg-gray-50 text-primary h-8">
-            <p class="text-primary text-sm">View users</p>
-        </div>
-    </div>
-    
-
-    <div class=" flex-1 flex-col-1 divide-y divide-gray-lightbg-white rounded shadow-md self-center">
-        <div class="flex-col space-y-1 py-4 px-2 h-24">
-            <p class="text-gray-light text-base">Users</p>
-            <p class="text-lg text-gray">{{ count($users) }}</p>
-        </div>
-        <div class="flex p-2 bg-gray-50 text-primary h-8">
-            <p class="text-primary text-sm">View users</p>
-        </div>
-    </div>
-    
-    
 </div>
