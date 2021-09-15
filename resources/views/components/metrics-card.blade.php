518e4362d145b680c670a93ff5224957790b1c7e
tijjvni <tijjvni@gmail.com>
2021-09-15 08:49:23 +0000

    updates

--- resources/views/components/metrics-card.blade.php
+++ resources/views/components/metrics-card.blade.php
@@ -1,7 +1,7 @@
 <div {{ $attributes->merge(['class' => 'flex-1 bg-white p-3 rounded shadow-md self-center h-28 my-2 md:mb-0']) }}>
     <div class="flex flex-row ">
 		<div class="h-20 w-20 mr-1 inline-flex items-center justify-center rounded-full text-secondary flex-shrink-0 ">
-			<i class="fa fa-{{ $icon }} text-3xl"></i>
+			<i class="fa fa-user text-3xl"></i>
 		</div> 
 		<div class="flex-grow text-secondary">
 			<p class="text-base">Title</p>
