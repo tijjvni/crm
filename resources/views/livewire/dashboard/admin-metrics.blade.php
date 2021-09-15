0f9f16435923d44d2f9b94879f1974841ca23fb2
tjyusuf <tjyusuf@Yahoo.com>
2021-09-15 14:46:29 +0000

    updates

--- resources/views/livewire/dashboard/admin-metrics.blade.php
+++ resources/views/livewire/dashboard/admin-metrics.blade.php
@@ -1,11 +1,43 @@
-<div>
-    <div class="flex-col divide-y rounded shadow divide-gray-light">
+<div class="flex flex-wrap space-x-2 space-y-2">
+<div class="divide-y divide-gray-light flex-1 flex-col-1 bg-white p-3 rounded shadow-md self-center">
         <div class="flex space-y-1 py-4 px-2 h-24">
             <p class="text-gray-light text-base">Users</p>
             <p class="text-lg text-gray">{{ count($users) }}</p>
         </div>
-        <div class="flex p-2 bg-gray-50 text-primary">
+        <div class="flex p-2 bg-gray-50 text-primary h-8">
             <p class="text-primary text-sm">View users</p>
         </div>
     </div>
+
+    <div class="divide-y divide-gray-light flex-1 flex-col-1 bg-white p-3 rounded shadow-md self-center">
+        <div class="flex space-y-1 py-4 px-2 h-24">
+            <p class="text-gray-light text-base">Users</p>
+            <p class="text-lg text-gray">{{ count($users) }}</p>
+        </div>
+        <div class="flex p-2 bg-gray-50 text-primary h-8">
+            <p class="text-primary text-sm">View users</p>
+        </div>
+    </div>
+
+    <div class="divide-y divide-gray-light flex-1 flex-col-1 bg-white p-3 rounded shadow-md self-center">
+        <div class="flex space-y-1 py-4 px-2 h-24">
+            <p class="text-gray-light text-base">Users</p>
+            <p class="text-lg text-gray">{{ count($users) }}</p>
+        </div>
+        <div class="flex p-2 bg-gray-50 text-primary h-8">
+            <p class="text-primary text-sm">View users</p>
+        </div>
+    </div>
+
+    <div class="divide-y divide-gray-light flex-1 flex-col-1 bg-white p-3 rounded shadow-md self-center">
+        <div class="flex space-y-1 py-4 px-2 h-24">
+            <p class="text-gray-light text-base">Users</p>
+            <p class="text-lg text-gray">{{ count($users) }}</p>
+        </div>
+        <div class="flex p-2 bg-gray-50 text-primary h-8">
+            <p class="text-primary text-sm">View users</p>
+        </div>
+    </div>
+
+
 </div>
