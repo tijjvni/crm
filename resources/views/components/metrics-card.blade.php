64fcec0127d469c751a3f0462bc9cd927305fee9
tijjvni <tijjvni@gmail.com>
2021-09-15 09:18:05 +0000

    updates

--- resources/views/components/metrics-card.blade.php
+++ resources/views/components/metrics-card.blade.php
@@ -1,11 +1,27 @@
 <div {{ $attributes->merge(['class' => 'flex-1 bg-white p-3 rounded shadow-md self-center h-28 my-2 md:mb-0']) }}>
-    <div class="flex flex-row">
-		<div class="h-20 w-20 mr-1 inline-flex items-center justify-center rounded-full text-secondary flex-shrink-0 ">
-			<i class="fa fa-user text-3xl"></i>
-		</div> 
-		<div class="flex-grow text-secondary">
-			<p class="text-base">Title</p>
-			<h3 class="text-2xl text-primary font-semibold">100</h3>
-		</div>
-	</div>	
+
+                <div
+                  class="p-3 mr-4 text-orange-500 bg-gray-100 rounded-full "
+                >
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                      d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
+                    ></path>
+                  </svg>
+                </div>
+                <div>
+                  <p
+                    class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
+                  >
+                    Total clients
+                  </p>
+                  <p
+                    class="text-lg font-semibold text-gray-700 dark:text-gray-200"
+                  >
+                    6389
+                  </p>
+                </div>
+
+                
+
 </div>
