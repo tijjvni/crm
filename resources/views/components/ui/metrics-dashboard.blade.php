f8686a7dbdb86e14710d28e6bc8d53bb3dc6e95d
tijjvni <tijjvni@gmail.com>
2021-09-15 11:12:39 +0000

    updates

--- resources/views/components/ui/metrics-dashboard.blade.php
+++ resources/views/components/ui/metrics-dashboard.blade.php
@@ -1,6 +1,6 @@
 <div {{ $attributes->merge(['class' => 'flex-1 bg-white p-3 rounded shadow-md self-center h-28 my-2 md:mb-0']) }}>
     <div class="flex flex-row">
-		<div class="h-20 w-20 mr-1 inline-flex items-center justify-center flex-shrink-0 text-gray-400 bg-gray-50 rounded-full">
+		<div class="h-20 w-20 mr-1 inline-flex items-center justify-center flex-shrink-0 text-gray bg-gray-50 rounded-full">
             <i class="fa fa-{{$icon}} text-3xl"></i>		
 		</div> 
 		<div class="flex-grow text-secondary">
